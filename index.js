a877d60731a8a6f5a4cad4f4e1fadd217d534f79
Kaleb <Kalebalebachew4@gmail.com>
2023-12-19 21:53:41 +0300

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const app = express()
+
+
+
+
+port = process.env.PORT || 5000
+app.listen(console.log(`server listening on ${port} `))
